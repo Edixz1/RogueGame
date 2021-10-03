e78629ebac72888fd7a2f68de0ad7bf2d15d0449
Edixz1 <57773548+Edixz1@users.noreply.github.com>
2021-10-03 02:57:31 -0400

    Procedural PRogressss

--- RogueGame/Assets/Scripts/ProceduralCode/GridManagement.cs
+++ RogueGame/Assets/Scripts/ProceduralCode/GridManagement.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class GridManagement : MonoBehaviour
 {
     public int width, height;
+    public GameObject[] tilePossibillity;
+    public GameObject tilePrefab;
+    public GameObject tlieSize;
+
     private float cellWidth;
     private float cellHeight;
-    public GameObject tilePrefab;
-
     void GenerateGrid()
     {
         for (int x = 0; x < width; x++)
@@ -24,9 +26,11 @@ public class GridManagement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        cellWidth = tilePrefab.transform.localScale.x;
-        cellHeight = tilePrefab.transform.localScale.y;
+        cellWidth = tlieSize.transform.localScale.x;
+        cellHeight = tlieSize.transform.localScale.y;
+
+        //int rand = Random.Range(0, tilePossibillity.Length);
+
         GenerateGrid();
-        Debug.Log(cellWidth + " : " + cellHeight);
     }
 }
