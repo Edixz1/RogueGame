724b32c57bf1e40f6a9221cebe8773b5d1cfa077
macorm <71089879+macorm@users.noreply.github.com>
2021-11-01 11:38:25 -0400

    doors

---
+++ RogueGame/Assets/Scripts/DoorscriptComponent.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorscriptComponent : MonoBehaviour
+{
+    public int ennemiesAlive;
+    public bool ennemiesAreAlive;
+    public GameObject Ennemies;
+    public GameObject Doors;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Doors = this.transform.Find("Doors").gameObject;
+        GameObject map = this.gameObject;
+        GameObject Spawner = this.transform.Find("Spawners").gameObject;
+        Ennemies = Spawner.transform.GetChild(0).gameObject;
+        ennemiesAlive = Ennemies.transform.childCount;
+        ennemiesAreAlive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < Ennemies.transform.childCount; i++)
+        {
+            if (Ennemies.transform.GetChild(i).gameObject.activeInHierarchy)
+            {
+                ennemiesAlive++;
+            }
+        }
+        if(ennemiesAlive == 0)
+        {
+            Doors.SetActive(false);
+        }
+        ennemiesAlive = 0;
+    }
+}
